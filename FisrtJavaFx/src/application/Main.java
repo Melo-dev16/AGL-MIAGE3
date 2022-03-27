93fd07374818355c92e3e50c9962489ad6bc1eae
lenovo <lenovo@DESKTOP-JJNT1CR>
2022-03-27 00:47:44 +0000

    # WARNING: head commit changed in the meantime
    creation de packages : controllers, images, interfaces

--- FisrtJavaFx/src/application/Main.java
+++ FisrtJavaFx/src/application/Main.java
@@ -1,20 +1,23 @@
 package application;
 	
 import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-
-import javafx.scene.Parent;
 import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+
 
 public class Main extends Application {
 	@Override
-	public void start(Stage primaryStage) {
+	public void start(Stage primaryStage) { 
 		try {
-			Parent root = FXMLLoader.load(getClass().getResource("MainScene.fxml"));
-			Scene scene = new Scene(root,400,400);
-			primaryStage.setTitle("Hello World");  
+			Parent root = FXMLLoader.load(getClass().getResource("/Interfaces/Accueil.fxml"));
+			Scene scene = new Scene(root);
 			primaryStage.setScene(scene);
+
+			Image Appicon= new Image(getClass().getResourceAsStream("/Images/ProjeQtor.png"));
+			primaryStage.getIcons().add(Appicon);
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
